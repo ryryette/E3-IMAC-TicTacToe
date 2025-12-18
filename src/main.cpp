004268a0e615196ae2385bea451d39cbd7770c52
ryryette <rvt.univ@gmail.com>
2025-12-18 22:59:09 +0100

    Ajout des cases

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,22 @@
 int main()
 {
+    
+/*
+Affichage du plateau de jeu
+Vous devez réaliser une fonction draw_game_board (vous pouvez modifier légèrement le nom de la fonction si vous le souhaitez) qui prend en paramètre un tableau de caractères (char) et affiche le plateau de jeu dans la terminal.
+Le plateau de jeu doit être affiché de la manière suivante :
+
+| 1 | 2 | 3 |
+| 4 | 5 | 6 |
+| 7 | 8 | 9 |
+
+où chaque chiffre représente une case du plateau de jeu.
+
+Le plateau doit être représenté dans le code sous forme d'un tableau de caractères (char) représentant les symboles des joueurs ou un caractère "vide" (par exemple un .) pour une case vide.
+
+⚠️ Ici le nombre de cases du plateau est connu à l'avance, vous devez donc de préférence utiliser un tableau statique plutôt qu'un tableau dynamique pour représenter le plateau de jeu.
+*/
+    char cases[9] {'1','2','3','4','5','6','7','8','9'};
+    
     return 0;
 }
